d3a785370b63bff2233e073b7951102833fbec13
Felixlimz <felixlim111@gmail.com>
2022-03-04 20:06:32 +0700

    update + security + sudah bisa login

--- src/main/java/com/miniproject/haimp004/controller/MainController.java
+++ src/main/java/com/miniproject/haimp004/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.miniproject.haimp004.controller;
 
+import com.miniproject.haimp004.data.Category;
 import com.miniproject.haimp004.data.User;
 import com.miniproject.haimp004.repository.UserRepository;
+import com.miniproject.haimp004.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +18,8 @@ import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    private CategoryService categoryService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Model model, String error, String logout){
@@ -25,6 +30,16 @@ public class MainController {
             model.addAttribute("msg", "You have been logged out successfully.");
 
         return "login";
+    }
+
+    @RequestMapping(value = "/test")
+    public String testPage(Model model){
+        List<Category> listCategory = categoryService.listAll();
+        model.addAttribute("listCategory", listCategory);
+
+        Category category = new Category();
+        model.addAttribute("category", category);
+        return "product-category";
     }
 //    @Autowired
 //    private UserRepository userRepository;
