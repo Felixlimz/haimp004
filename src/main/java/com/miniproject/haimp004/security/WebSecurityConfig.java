917862675380c76b755fce6883e510f6bab74175
Felixlimz <felixlim111@gmail.com>
2022-03-05 23:09:12 +0700

    muantab

--- src/main/java/com/miniproject/haimp004/security/WebSecurityConfig.java
+++ src/main/java/com/miniproject/haimp004/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.miniproject.haimp004.security;
 
 import com.miniproject.haimp004.service.CustomUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,13 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private DataSource dataSource;
 
     @Bean
     public UserDetailsService userDetailsService() {
