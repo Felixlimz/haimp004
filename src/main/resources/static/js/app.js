eb87ebd97a0e9e94de4b150c0d64d13aa40a2471
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2022-03-09 10:53:39 +0700

    up

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,3 +6,22 @@ function check_pass() {
         document.getElementById('submit').disabled = true;
     }
 }
+
+function func() {
+  if (document.getElementById("stock").value == 0) {
+        document.getElementById('submit').style.display = "none";
+        document.getElementById('message').style.display = "";
+  } else {
+        document.getElementById('submit').style.display = "";
+        document.getElementById('message').style.display = "none";
+  }
+}
+
+//$(document).ready(function () {
+//    if($('#stock').val() == 0)
+//    {
+//       document.getElementById('submit').disabled = true;
+//    } else {
+//        document.getElementById('submit').disabled = false;
+//    }
+//});
